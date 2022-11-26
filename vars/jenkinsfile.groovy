0a55e158a86d2696cc990aecfa2b01b93cb24827
Sudheersanju <116295456+Sudheersanju@users.noreply.github.com>
2022-11-26 15:57:50 +0530

    Update jenkinsfile.groovy

--- vars/jenkinsfile.groovy
+++ vars/jenkinsfile.groovy
@@ -10,3 +10,7 @@ def deploy(a,b)
 {
   sh 'scp /var/lib/jenkins/workspace/${a}/webapp/target/webapp.war ubuntu@172.31.27.110:/var/lib/tomcat9/webapps/${b}.war'
 }
+def run(c)
+{
+  sh 'sh java -jar /var/lib/jenkins/workspace/${c}/testing.jar'
+}
