7972b6111c46e8251b0a16c43c4e294fdd2e8d07
Sudheersanju <116295456+Sudheersanju@users.noreply.github.com>
2022-11-26 15:02:04 +0530

    Create jenkinsfile.groovy

---
+++ vars/jenkinsfile.groovy
@@ -0,0 +1,11 @@
+def download(repo)
+{
+  git "${repo}"
+}
+def build()
+{
+  sh 'mvn package'
+}
+def deploy(a,b)
+{
+  sh 'scp /var/lib/jenkins/workspace/${a}/webapp/target/webapp.war ubuntu@172.31.27.110:/var/lib/tomcat9/webapps/${b}.war'
